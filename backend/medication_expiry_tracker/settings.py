e1492991a65e34895742bcf9b89f118fe4fcf3b6
Doseman01 <olamidekuddus@gmail.com>
2025-04-07 23:48:48 +0100

    dbupdate

--- backend/medication_expiry_tracker/settings.py
+++ backend/medication_expiry_tracker/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+from decouple import config
 
 from pathlib import Path
 
@@ -75,10 +76,15 @@ WSGI_APPLICATION = 'medication_expiry_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),   
+        'PORT': config('DB_PORT', default='3306'),       
     }
 }
+
 
 
 # Password validation
